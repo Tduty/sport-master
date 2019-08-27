05954291ae119b609d726c8325aec8a51a2b0e64
Evgeny Mezentsev <eimezentsev@yandex.ru>
2019-08-27 17:44:09 +0300

    Added toolbar and bottom navigation bar

--- app/src/main/java/info/tduty/sport_master/MainActivity.kt
+++ app/src/main/java/info/tduty/sport_master/MainActivity.kt
@@ -2,11 +2,13 @@ package info.tduty.sport_master
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toolbar.setTitle(R.string.app_name)
     }
 }
