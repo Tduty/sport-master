8cafdb11793d16d9dd906cb034ba2737df3656ef
Evgeny Mezentsev <eimezentsev@yandex.ru>
2019-09-02 16:16:23 +0300

    Added androidx Fragment and Activity

--- app/src/main/java/info/tduty/sport_master/ui/MainActivity.kt
+++ app/src/main/java/info/tduty/sport_master/ui/MainActivity.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import info.tduty.sport_master.R
 import kotlinx.android.synthetic.main.toolbar.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         toolbar.setTitle(R.string.app_name)
     }
 }
--- app/src/main/java/info/tduty/sport_master/ui/training/TrainingFragment.kt
+++ app/src/main/java/info/tduty/sport_master/ui/training/TrainingFragment.kt
@@ -1,44 +1,20 @@
 package info.tduty.sport_master.ui.training
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import info.tduty.sport_master.R
 import kotlinx.android.synthetic.main.fragment_training.*
 
-class TrainingFragment : Fragment() {
+class TrainingFragment : Fragment(R.layout.fragment_training) {
 
     private var adapter: TrainingAdapter? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_training, container, false)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         adapter = TrainingAdapter()
         rv_training.layoutManager = LinearLayoutManager(requireContext())
         rv_training.adapter = adapter
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
-    override fun onDetach() {
-        super.onDetach()
     }
 }
